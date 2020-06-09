fb08db9119f711a107315416125122403fdd1cd9
Sami ul Haq <64400874+sami-ul-haq@users.noreply.github.com>
2020-06-09 23:50:40 +0500

    Materialize CSS Practice

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+    $('.carousel').carousel();
+
+
+
+    
+  });
+         
